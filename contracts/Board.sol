f3f9cb214a9f57c87fe9dff494e8aebcf91b23fc
Dave Anderson <davepander@gmail.com>
2019-01-27 21:40:39 -0500

    Revert "Fixed OpenZeppelin path structure"
    This reverts commit 91c768d64ec0a5a64f40809d1735ae781d4c83a1.

--- contracts/Board.sol
+++ contracts/Board.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.5.0;
 
 contract Board {
 
-    address private owner;
-
     constructor() public {
         /* Set the owner to the creator of this contract */
         owner = msg.sender;
@@ -16,22 +14,22 @@ contract Board {
     mapping(address => bool) boardMembers;
 
     modifier onlyBoard {
-      require(boardMembers[msg.sender] == true || msg.sender == owner);
+      require(boardMembers[msg.sender] == true);
       _;
     }
 
     function addBoardMem(address _m)
-      public
+      internal
       onlyBoard
       returns(bool) {
         boardMembers[_m] = true;
-        emit NewBoardMember(_m);
+        emit NewBoardMember(_m); 
         return true;
       }
 
 
     //This function allows for the destruction of the contract
-    /*function kill()
+    function kill()
     public
     {
       if(msg.sender == owner){
@@ -39,7 +37,7 @@ contract Board {
         _;
       }
     }
-    */
+
 
     //
     // State variables
