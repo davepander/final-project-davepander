06fd183cf46455254aa728021954b5a2bf3b0ba7
Dave Anderson <davepander@gmail.com>
2019-01-28 05:55:10 -0500

    Update Meeting and Recompile

---
+++ Documentation/CommonAttacks.md
@@ -0,0 +1,21 @@
+## Common Attacks
+
+This dApp is not focused on value transfer and/or monetary exchange. Therefor the attack vectors are reduced.
+
+# Role Protection
+
+- Ownership of the Board is restricted to the Chairperson
+- Ownership is further enforced through OpenZeppelin Ownable libraries
+- BoardMembers can be verified (eventually through uPort)
+
+# Overflow Protection
+Loops and incremental functions are avoided
+
+# ReEntrancy
+Care has been taken, and implementing OpenZeppelin Reentrancy Guard in combination with ownership controls will limit the risks.
+
+# Timestamp
+The contracts are meant to be executed at "human" speeds so race situations are a low priority. We are realying on the transaction records on the blockchain to cover relevant real-world timestamp inquiries.
+
+#DOS
+If there are DOS attacks, Breaker Circuits for the Meeting and Motion (the most time sensitive relative to the Board contract) are in place.  These Circuits are under the contrl of the Chairperson (and are implemented with the OpenZeppelin Ownable contract).
--- contracts/Meeting.sol
+++ contracts/Meeting.sol
@@ -1,17 +1,43 @@
 pragma solidity ^0.5.0;
 
-import './MeetingBreaker.sol';
+// import './MeetingBreaker.sol';
 // import 'jsmnsol-lib/JsmnSolLib.sol';
+// import './Motion.sol';
 
 contract Meeting {
-    // A dynamically-sized array of `Motion` structs.
-    // Proposal[] public motions;
+
+    address chairPerson;
+    enum State { Called, Adjourned } // Enum
+
+    // An array of `Motion` structs.
+    //Motion[] public motions;
+    //Present[] public address;
 
     /** @dev Set the owner to the creator of this contract */
     constructor() public {
-
-        address owner = msg.sender;
+        chairPerson = msg.sender;
     }
 
-    enum State { Called, Adjourned } // Enum
+    /*
+    function checkIn() public {
+        //Function for Board Members to check-in to the meeting.
+    };*/
+
+    /*
+    function getAgenda () public {
+      //Function for retrieving and parsing the Agenda JSON object into
+      an array of Motion calls.
+    };
+    */
+
+    /*function Adjourn () public returns(bool) {
+      // Check to see if all proposals are done
+
+      // If not complete
+
+      // If complete Return
+
+
+    }*/
+
 }
